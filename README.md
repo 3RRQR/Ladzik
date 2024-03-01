5b87d5a565f7b73ba415c0857c9b3741c98b1a81
3RRQR <errormielczarek2004@gmail.com>
2024-03-01 20:35:49 +0100

    deleting mentions of attachments from app.py, editting readme.md

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Ladzik - remote controlled robot using web browser
 <p>robot named "Ladzik" is really just a raspberry pi on crawler platform. It has an attached a raspberry pi camera v2 on front and a lipo battery mounted underneath a platform for vision and power.</p> 
 
-<h2 style="color:red">This branch has the code edited so that it can be run on a raspberry pi without a camera - all mention of the camera has been removed, and the livefeed on the site has a great video of Ladzik's future mission.</h2>
+
+<h2 style="color:red">This branch has the code edited so that it can be run on a raspberry pi without a camera and motor driver HAT - all mention of the camera, HAT has been removed, and the livefeed on the site has a great video of Ladzik's future mission. Sterring buttons do nothing</h2>
 <p>The implications of these changes go as far as the performance as well as the size on disk of the entire code. This makes merging this branch to the main branch imposible and will never happen.Once the entire project is finished this branch WILL BE DELETED</p>
--- app.py
+++ app.py
@@ -1,43 +1,10 @@
-from flask import Flask, Response, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify
 import io
 import threading
-import picamera2
-from picamera2.encoders import JpegEncoder
-from picamera2.outputs import FileOutput
-from libcamera import Transform
-from main import driving, MotorDriver
 app = Flask(__name__)
-drive = driving
-Motor = MotorDriver()
 button_states = {}
-picam2 = picamera2.Picamera2()
 
-class StreamingOutput(io.BufferedIOBase):
-    def __init__(self):
-        self.frame = None
-        self.condition = threading.Condition()
-
-    def write(self, buf):
-        with self.condition:
-            self.frame = buf
-            self.condition.notify_all()
-
-def camera_stream(output):
-    config = picam2.create_video_configuration(main={"size": (1280, 720)}, controls={"FrameRate": (30.0)}, transform=Transform(vflip=True))
-    picam2.configure(config)
-    picam2.start_recording(JpegEncoder(), FileOutput(output))
-    try:
-        while True:
-            with output.condition:
-                output.condition.wait()
-                frame = output.frame
-            yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-    finally:
-        picam2.stop_recording()
-
-@app.route('/stream.mjpg')
-def video_feed():
-    return Response(camera_stream(output), mimetype='multipart/x-mixed-replace; boundary=frame')
+#   WEBSITE
 
 @app.route('/download')
 def down():
@@ -61,34 +28,23 @@ def buttonsUpdate():
     button_states[button] = is_pressed
     
     if button == "up":
-        drive.forward(100)
         print(button, is_pressed)
     elif button == "down":
-        drive.backward(100)
         print(button, is_pressed)
     elif button == "left":
-        drive.left(100)
         print(button, is_pressed)
     elif button == "right":
-        drive.right(100)
         print(button, is_pressed)
     elif button == "left-cat":
-        drive.leftAlt(100)
         print(button, is_pressed)
     elif button == "right-cat":
-        drive.rightAlt(100)
         print(button, is_pressed)
     elif button == "left-cat-back":
-        drive.leftAltBack(100)
         print(button, is_pressed)
     elif button == "right-cat-back":
-        drive.rightAltBack(100)
         print(button, is_pressed)
     
-
-    if (button =="stop") or (is_pressed == False):
-        drive.stop()
-
+        
     return jsonify({'status': 'success'})
 
 
@@ -106,35 +62,16 @@ def JoystickUpdate():
         turnspeed = abs(posY) - abs(posX)
     else:
         turnspeed = 0
-    
-    if posX < 0 and posY > 0:
-        Motor.MotorRun(0, 'forward', turnspeed)
-        Motor.MotorRun(1, 'forward', posY)
-    if posX > 0 and posY > 0:
-        Motor.MotorRun(0, 'forward', posY)
-        Motor.MotorRun(1, 'forward', turnspeed)
-    if posX < 0 and posY < 0:
-        Motor.MotorRun(0, 'backward', turnspeed)
-        Motor.MotorRun(1, 'backward', abs(posY))
-    if posX > 0 and posY < 0:
-        Motor.MotorRun(0, 'backward', abs(posY))
-        Motor.MotorRun(1, 'backward', turnspeed)
+
     if posX < 20 and posX > -20 and posY > 0:
-        drive.forward(posY)
         print("STR JOY forward")
     if posX < 20 and posX > -20 and posY < 0:
-        drive.backward(abs(posY))
         print("STR JOY backward")
     if posX < 0 and posY > -20 and posY < 20:
-        drive.left(abs(posX))
         print("STR JOY left")
     if posX > 0 and posY > -20 and posY < 20:
-        drive.right(abs(posX))
         print("STR JOY right")
 
-    if posX == 0 and posY == 0:
-        Motor.MotorStop(0)
-        Motor.MotorStop(1)
 
     return jsonify({'status': 'success'})
 
@@ -168,13 +105,8 @@ def sliders():
 
 
 if __name__ == '__main__':
-    
-    output = StreamingOutput()
-    stream_thread = threading.Thread(target=camera_stream, args=(output,))
-    stream_thread.start()
 
     app.run(host='0.0.0.0', port=8000, threaded=True)
-    stream_thread.join()
 
 
 
